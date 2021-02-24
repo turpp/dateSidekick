73a8823db65c0fc920771e10fb52458a1d9be9a3
turpp <white.tripp@icloud.com>
2021-02-24 07:51:47 -0600

    added functionality for random that generates the foodSelection and foodActivity just like custom and added a form on custom for zip code

--- src/dateContainer/Custom.js
+++ src/dateContainer/Custom.js
@@ -6,13 +6,33 @@ import FoodActivity from './FoodActivity'
 
 export default class Custom extends React.Component{
     state={
-        dateType: ''
+        dateType: '',
+        zipcode: ''
     }
 
     handleClick=(event)=>{
         this.setState({
             dateType: event.target.id
         })
+    }
+
+    handleChange=(event)=>{
+        this.setState({
+            zipcode: event.target.value
+        })
+    }
+
+    handleSubmit=(event)=>{
+        event.preventDefault()
+        switch(this.state.dateType){
+            case 'food':
+                return <Food/>
+            case 'food-activity':
+                return <FoodActivity/>
+            default:
+                return <DateTemplates handleClick={this.handleClick}/>
+        }
+
     }
 
     renderTemplate=()=>{
@@ -28,7 +48,15 @@ export default class Custom extends React.Component{
     render(){
         return <div>
             {this.renderTemplate()}
+            <form onSubmit={this.handleSubmit}>
+                <label>Zipcode:</label>
+                <input type='number' value={this.state.zipcode} onChange={this.handleChange}/>
+                <input type='submit'/>
+            </form>
+            
             </div>
         // return <p>Hi</p>
+
+        //can put a if statement to render the buttons and also zipcode and once both submit render the swtich statement
     }
 }
--- src/dateContainer/Random.js
+++ src/dateContainer/Random.js
@@ -1,7 +1,56 @@
 import React from 'react'
+import DateTemplates from '../datePresentation/DateTemplates'
+import ActivitySelection from '../datePresentation/ActivitySelection'
+import FoodSelection from '../datePresentation/FoodSelection'
+
 
 
 export default class Random extends React.Component{
+    state={
+        dateType: '',
+        dateFood: '',
+        dateActivity: ''
+    }
+
+    renderFoodSelction=()=>{
+        return this.state.foodResults.map(result=>{
+            return <FoodSelection result={result} addFoodToDate={this.addFoodToDate}/>
+        })
+
+    }
+
+    renderActivitySelection=()=>{
+        return this.state.activityResults.map(result=>{
+            return <ActivitySelection result={result} addActivityToDate={this.addActivityToDate}/>
+        })
+    }
+
+    addFoodToDate=(event)=>{
+        debugger
+        this.setState({
+            dateFood: event.target.value
+        })
+    }
+
+    addActivityToDate=(event)=>{
+        this.setState({
+            dateActivity: event.target.value
+        })
+    }
+
+
+
+
+    renderTemplate=()=>{
+        switch(this.state.dateType){
+            case 'food':
+                return <FoodSelection/>
+            case 'food-activity':
+                return <ActivitySelection/>
+            default:
+                return <DateTemplates handleClick={this.handleClick}/>
+        }
+    }
 
 
     render(){
