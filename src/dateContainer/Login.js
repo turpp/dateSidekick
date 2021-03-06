671a9bcb238f039fcf36535c732d1d75adfce56b
turpp <white.tripp@icloud.com>
2021-03-06 14:37:41 -0600

    styled login abd sign up

--- src/dateContainer/Login.js
+++ src/dateContainer/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {login} from '../redux/actions/authActions'
 import {connect} from 'react-redux'
+import {Form,Card, Button, Container,Row} from 'react-bootstrap'
 
 class Login extends React.Component{
 
@@ -21,14 +22,31 @@ class Login extends React.Component{
     }
 
     render(){
-        return <div>
-            <form onSubmit={this.handleSubmit}>
-                <label>Username:</label>
-                <input name='username' type='text' value={this.state.username} placeholder='username' onChange={this.handleChange}/>
-                <label>Password:</label>
-                <input name='password' type='password' value={this.state.password} placeholder='password' onChange={this.handleChange}/>
-                <input type='submit'/>
-            </form>
+        return <div id='login'>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <Container>
+                
+            <Row className="justify-content-md-center">
+            <Card style={{width: '35em'}} className="card border-info mb-3 shadow-lg p-3 mb-5 bg-body rounded">
+                <Card.Header>
+                    <h4>Welcome back!</h4>
+                </Card.Header>
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                <Form.Label>Username:</Form.Label>
+                <Form.Control name='username' type='text' value={this.state.username} placeholder='username' onChange={this.handleChange}/>
+                <Form.Label>Password:</Form.Label>
+                <Form.Control name='password' type='password' value={this.state.password} placeholder='password' onChange={this.handleChange}/>
+                </Form.Group>
+                <Button type='submit' >Login</Button>
+
+            </Form>
+            </Card>
+            </Row>
+            </Container>
         </div>
     }
 }
