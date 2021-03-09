269be6f61f55b40cde86a44ab531f3c28f97e513
turpp <white.tripp@icloud.com>
2021-03-08 23:58:44 -0600

    fixed error of a tag in a tag.

--- src/dateContainer/Navbar.js
+++ src/dateContainer/Navbar.js
@@ -4,39 +4,41 @@ import {logout} from '../redux/actions/authActions'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import {Navbar,Nav, Row, Container, Col} from 'react-bootstrap'
+import '../App.css';
+
 class Navbarr extends React.Component{
     render(){
         return  <Navbar bg="light" variant="light" >
             <Navbar.Brand><Link to='/'>Date Sidekick</Link></Navbar.Brand>
             <Nav className="mr-right">
             <Nav.Item>
-                <Nav.Link><Link to='/random'>Random Outing</Link></Nav.Link>
+                <Link to='/random'>Random Outing</Link>
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link><Link to='/custom'>Desgin Outing</Link></Nav.Link>
+                <Link to='/custom'>Desgin Outing</Link>
             </Nav.Item>
 
             {this.props.loggedIn?(
                 <>
             <Nav.Item>
-                <Nav.Link><Link to='/profile'>Profile</Link></Nav.Link>
+                <Link to='/profile'>Profile</Link>
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link><Link to='' onClick={()=>this.props.logout(this.props.history)}>Logout</Link></Nav.Link>
+                <Link to='' onClick={()=>this.props.logout(this.props.history)}>Logout</Link>
             </Nav.Item>
 
                 </>
             ) : (
                 <>
             <Nav.Item>
-                <Nav.Link><Link to='/signup'>Signup</Link></Nav.Link>
+                <Link to='/signup'>Signup</Link>
             </Nav.Item>
 
                 {/* <Link to='/signup'>Signup</Link> */}
             <Nav.Item>
-                <Nav.Link><Link to='/login'>Login</Link></Nav.Link>
+                <Link to='/login'>Login</Link>
             </Nav.Item>
 
                 
