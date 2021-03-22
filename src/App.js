cf0bf1e732876a4a3782e71639eca978b5efcb00
turpp <white.tripp@icloud.com>
2021-03-21 21:21:27 -0500

    refactored random date into one component

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ import {checkLoggedIn} from './redux/actions/authActions'
 import {connect} from 'react-redux'
 import LoaderWheel from './datePresentation/LoaderWheel'
 import {Container} from 'react-bootstrap'
-import RandomFood from './datePresentation/RandomFood'
-import RandomFoodActivity from './datePresentation/RandomFoodActivity'
-
+import RandomDate from './datePresentation/RandomDate'
 class App extends React.Component {
   
   state={
@@ -43,8 +41,8 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={Home}/>
             <Route exact path='/random-date/new' component={Random}/>
-            <Route path='/random-food-dates/:zipcode' render={routerProps=><RandomFood {...routerProps}/>}/>
-            <Route path='/random-food-activity-dates/:zipcode' render={routerProps=><RandomFoodActivity {...routerProps}/>}/>
+            <Route path='/random-food-dates/:zipcode' render={routerProps=><RandomDate {...routerProps} type='food'/>}/>
+            <Route path='/random-food-activity-dates/:zipcode' render={routerProps=><RandomDate {...routerProps} type='food-activity'/>}/>
             <Route exact path='/custom-date/new' component={Custom}/>
             <Route exact path='/login' component={Login}/>
             <Route exact path='/user/:id'render={props=>{
--- src/datePresentation/RandomDate.js
+++ src/datePresentation/RandomDate.js
@@ -1,23 +1,8 @@
-// import React from 'react'
-// import {Row, Col} from 'react-bootstrap'
-// import Food from '../dateContainer/Food'
-// import Activity from '../dateContainer/Activity'
-
-// function RandomFoodActivity(props){
-//     return <Row><Col><Food zipcode={props.zipcode} type='food'  random='true'/></Col>+<Col><Activity zipcode={props.zipcode} type='food-activity' random={'true'} /></Col></Row>
-// }
-
-// export default RandomFoodActivity
-
-
-
 import React from 'react'
 import {Row, Container, CardDeck, Jumbotron, Button,Col} from 'react-bootstrap'
 import SelectionType from '../dateContainer/SelectionType'
 
-
-
-class RandomFoodActivity extends React.Component{
+class RandomFood extends React.Component{
     state={
         reloader: Math.random()
     }
@@ -26,21 +11,20 @@ class RandomFoodActivity extends React.Component{
             this.props.history.push('/random-date/new')
             return null
         }else{
-
     return <Jumbotron>
     <Container fluid>
     <Row className="justify-content-md-center">
         <CardDeck>
-        <Row><Col><SelectionType zipcode={this.props.match.params.zipcode} type='food'  random='true'/></Col>+<Col><SelectionType zipcode={this.props.match.params.zipcode} type='food-activity' random={'true'} /></Col></Row>
+            {this.props.type==='food'?<Row><SelectionType zipcode={this.props.match.params.zipcode} type='food'  random='true'/></Row> : <Row><Col><SelectionType zipcode={this.props.match.params.zipcode} type='food'  random='true'/></Col>+<Col><SelectionType zipcode={this.props.match.params.zipcode} type='food-activity' random={'true'} /></Col></Row>}
         </CardDeck>
     </Row>
 </Container>
 <Button variant='info' onClick={()=>this.setState({reloader: Math.random()})}>Try Again</Button>{'   '}
 <Button variant='info' onClick={()=>this.props.history.push('/random-date/new')}>Start Over</Button>
 </Jumbotron>
+        }
     }
-}
 
 }
 
-export default RandomFoodActivity
+export default RandomFood
