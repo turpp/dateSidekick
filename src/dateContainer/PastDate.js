f9a39390414df420b101fa44911192740d45e767
turpp <white.tripp@icloud.com>
2021-03-25 15:25:29 -0500

    pushing updates to heroku

--- src/dateContainer/PastDate.js
+++ src/dateContainer/PastDate.js
@@ -12,7 +12,8 @@ import SelectionCard from '../datePresentation/SelectionCard'
     state={
         show: false,
         notes: this.props.date.notes,
-        date: this.props.date.date
+        date: this.props.date.date,
+        likes: 0
     }
 
     handleClose= ()=>{
@@ -30,6 +31,13 @@ import SelectionCard from '../datePresentation/SelectionCard'
     handleChange=(event)=>{
         this.setState({
             [event.target.name]: event.target.value
+        })
+    }
+
+    handleLike=()=>{
+        let likes=this.state.likes
+        this.setState({
+            likes: likes + 1
         })
     }
 
@@ -51,6 +59,8 @@ import SelectionCard from '../datePresentation/SelectionCard'
                             <h5 style={{textAlign: 'left'}}>Notes:<br></br>{!!this.props.date.notes ? this.props.date.notes: '- Tell me about it?!?!?'}</h5>
                         </Card.Body>
                         <Card.Footer>
+                            {/* add a like button. On button have an event listner to change liked amount */}
+                            <Button variant='primary' onClick={this.handleLike}>Like me! {this.state.likes}</Button>
                             <Button variant="info" onClick={this.handleShow}>Edit Date</Button>
                             <Button variant='danger' onClick={()=>this.props.deleteDate(this.props.date, this.props.history)}>Delete Date</Button>
                         </Card.Footer>
@@ -72,6 +82,8 @@ import SelectionCard from '../datePresentation/SelectionCard'
                         <h5 style={{textAlign: 'left'}}>When: <br></br>{!!this.props.date.date ? this.renderPrettyDate(): 'Add me!'}</h5>
                         <h5 style={{textAlign: 'left'}}>Notes:<br></br>{!!this.props.date.notes ? this.props.date.notes: '- Tell me about it?!?!?'}</h5>
                         <Card.Footer>
+                        <Button variant='primary' onClick={this.handleLike}>Like me! {this.state.likes}</Button>
+
                             <Button variant="info" onClick={this.handleShow}>Edit Date</Button>
                             <Button variant='danger' onClick={()=>this.props.deleteDate(this.props.date, this.props.history)}>Delete Date</Button>
                         </Card.Footer>
