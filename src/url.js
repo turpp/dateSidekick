580d2712910eedd62969ff7123a0f2541100a4f7
turpp <white.tripp@icloud.com>
2021-03-25 16:05:11 -0500

    removed dev url from fetch url

--- src/url.js
+++ src/url.js
@@ -2,5 +2,6 @@
    export const fetchUrl =()=>{
       const dev = "http://localhost:3000"
       const prod = "https://date-sidekick-api.herokuapp.com"
-      return process.env.NODE_ENV === 'development' ? dev : prod
+      // return process.env.NODE_ENV === 'development' ? dev : prod
+      return prod
    }
