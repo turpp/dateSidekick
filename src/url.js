aac6f81af0b2919f8ea8c124ad4335914a0d8fb6
turpp <white.tripp@icloud.com>
2021-03-25 15:56:57 -0500

    added api url to fetch url

--- src/url.js
+++ src/url.js
@@ -1,6 +1,6 @@
 
    export const fetchUrl =()=>{
       const dev = "http://localhost:3000"
-      const prod = "https://gentle-inlet-80267.herokuapp.com"
+      const prod = "https://date-sidekick-api.herokuapp.com"
       return process.env.NODE_ENV === 'development' ? dev : prod
    }
