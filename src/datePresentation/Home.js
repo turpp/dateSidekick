edab62e1711fd138473d5fddc2542c35114909c6
turpp <white.tripp@icloud.com>
2021-05-23 02:25:16 -0500

    made shadow bigger

--- src/datePresentation/Home.js
+++ src/datePresentation/Home.js
@@ -4,7 +4,7 @@ import {Jumbotron , Container, Button} from 'react-bootstrap'
 function Home(){
     return(
             <Container className='home-container' fluid>
-                <Container className='home-content shadow-sm'>
+                <Container className='home-content shadow-lg'>
                 <h1>Welcome to Date Sidekick!</h1>
                 <p>
                 Deciding where to go on a date can be tough. Date Sidekick has two ways it can help. With the help of Yelp you can plan you next date with a list of options from your choosen zipcode. You can create and save this date to your profile. For the adventurous folks we have random mode. This will give you a random place to go based on your zipcode!
