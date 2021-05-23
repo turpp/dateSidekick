c386e3d231a7ab63d7d8a8338c29c0ce80eb0fa4
turpp <white.tripp@icloud.com>
2021-05-23 00:26:35 -0500

    removed jumbtron and now centering a container

--- src/datePresentation/Home.js
+++ src/datePresentation/Home.js
@@ -3,16 +3,16 @@ import {Jumbotron , Container, Button} from 'react-bootstrap'
 
 function Home(){
     return(
-        <Jumbotron fluid>
-            <Container>
+            <Container className='home-container' fluid>
+                <Container className='home-content shadow'>
                 <h1>Welcome to Date Sidekick!</h1>
                 <p>
                 Deciding where to go on a date can be tough. Date Sidekick has two ways it can help. With the help of Yelp you can plan you next date with a list of options from your choosen zipcode. You can create and save this date to your profile. For the adventurous folks we have random mode. This will give you a random place to go based on your zipcode!
                 </p>
                 <Button variant="primary" onClick={()=>window.location.href='/random-date/new'}>Random Date</Button>{' '}
                 <Button variant="primary" onClick={()=>window.location.href='/custom-date/new'}>Create a custom date</Button>{' '}
+                </Container>
             </Container>
-        </Jumbotron>
     )
 }
 
