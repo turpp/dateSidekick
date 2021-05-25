c3ef55c9be1ed58094ac729bf831ee4860aade3b
turpp <white.tripp@icloud.com>
2021-05-25 00:02:50 -0500

    added margin to button on bottom for better view on mobile device

--- src/datePresentation/Home.js
+++ src/datePresentation/Home.js
@@ -9,8 +9,8 @@ function Home(){
                 <p>
                 Deciding where to go on a date can be tough. Date Sidekick has two ways it can help. With the help of Yelp you can plan you next date with a list of options from your choosen zipcode. You can create and save this date to your profile. For the adventurous folks we have random mode. This will give you a random place to go based on your zipcode!
                 </p>
-                <Button variant="primary" onClick={()=>window.location.href='/random-date/new'}>Random Date</Button>{' '}
-                <Button variant="primary" onClick={()=>window.location.href='/custom-date/new'}>Create a Custom Date</Button>{' '}
+                <Button style={{marginBottom: '1em'}} variant="primary" onClick={()=>window.location.href='/random-date/new'}>Random Date</Button>{' '}
+                <Button style={{marginBottom: '1em'}} variant="primary" onClick={()=>window.location.href='/custom-date/new'}>Create a Custom Date</Button>{' '}
                 </Container>
             </Container>
     )
