40f934a320c8b85d6e503603974558ab5906a6f3
turpp <white.tripp@icloud.com>
2021-05-25 00:02:07 -0500

    added title to dates and also did some styling to make it fit in card better

--- src/dateContainer/PastDate.js
+++ src/dateContainer/PastDate.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Card,Container, Row,Button} from 'react-bootstrap'
+import {Card,Container, Row,Button, Col} from 'react-bootstrap'
 import {deleteDate, editDate} from '../redux/actions/dateActions'
 import {withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
@@ -52,10 +52,12 @@ import SelectionCard from '../datePresentation/SelectionCard'
         switch(this.props.type){
             case('food'):
                 return <div>
-                    <Card style={{backgroundColor: '#FFFACD'}}>
+                    <Card style={{backgroundColor: '#FFFACD', padding: '1em', margin: 'auto', width: '350px'}}>
+                        <h2>Food Date</h2>
                         <Card.Body>
-                            <SelectionCard activity={this.props.date.activities[0]} addActivityToDate={''} random='true' />
-                            <h5 style={{textAlign: 'left'}}>When: <br></br>{!!this.props.date.date ? this.renderPrettyDate(): ' Add me!'}</h5>
+                            <SelectionCard activity={this.props.date.activities[0]} addActivityToDate={''} random='true' foodOnly={'past-food-date'} />
+                            <h5 style={{textAlign: 'left'}}>When: <strong>{!!this.props.date.date ? this.renderPrettyDate(): ''}</strong></h5>
+                            <hr></hr>
                             <h5 style={{textAlign: 'left'}}>Notes:<br></br>{!!this.props.date.notes ? this.props.date.notes: '- Tell me about it?!?!?'}</h5>
                         </Card.Body>
                         <Card.Footer>
@@ -72,20 +74,22 @@ import SelectionCard from '../datePresentation/SelectionCard'
                 </div>
             case('food-activity'):
                 return <div>
-                    <Card style={{backgroundColor: '#FFFACD'}}>
+                    <Card className='past-food-activity' style={{backgroundColor: '#FFFACD', padding: '1em' ,margin: 'auto', minWidth: '700px', width: 'fit-content', maxWidth: '700px'}}>
+                        <h2>Food and Activity Date</h2>
                         <Container>
-                            <Row>
+                                <Row lg={2} xs={1} md={1}>
                                 <SelectionCard activity={this.props.date.activities[0]} addActivityToDate='' random = 'true'/>
                                 <SelectionCard activity={this.props.date.activities[1]} addActivityToDate={''} random='true'/>
-                                </Row>
+                               </Row>
                         </Container>
-                        <h5 style={{textAlign: 'left'}}>When: <br></br>{!!this.props.date.date ? this.renderPrettyDate(): 'Add me!'}</h5>
+                        <h5 style={{textAlign: 'left'}}>When: <strong>{!!this.props.date.date ? this.renderPrettyDate(): ''}</strong></h5>
+                        <hr></hr>
                         <h5 style={{textAlign: 'left'}}>Notes:<br></br>{!!this.props.date.notes ? this.props.date.notes: '- Tell me about it?!?!?'}</h5>
                         <Card.Footer>
                         {/* <Button variant='primary' onClick={this.handleLike}>Like me! {this.state.likes}</Button> */}
 
-                            <Button variant="info" onClick={this.handleShow}>Edit Date</Button>
-                            <Button variant='danger' onClick={()=>this.props.deleteDate(this.props.date, this.props.history)}>Delete Date</Button>
+                            <Button variant="info" onClick={this.handleShow} style={{margin: '1%'}}>Edit Date</Button>
+                            <Button variant='danger' style={{margin: '1%'}} onClick={()=>this.props.deleteDate(this.props.date, this.props.history)}>Delete Date</Button>
                         </Card.Footer>
                     </Card>
                 <br></br>
