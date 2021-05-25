3a6dd9146ae9bde22c1cad5a84c9e40f620add03
turpp <white.tripp@icloud.com>
2021-05-25 00:00:06 -0500

    formatted with colums and containers. changed heading text and also added buttton to show past dates

--- src/dateContainer/Account.js
+++ src/dateContainer/Account.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col, Button, Fade, Collapse } from 'react-bootstrap'
 import {connect} from 'react-redux'
 import PastDate from './PastDate'
 import {fetchUrl} from '../url'
@@ -10,7 +10,8 @@ class Account extends React.Component{
     state={
         activities: [],
         dates: [],
-        loading: true
+        loading: true,
+        showDates: false
     }
     componentDidMount(){
         fetch(`${fetchUrl()}/users/${this.props.user.id}`).then(resp=>resp.json()).then(json=>{
@@ -34,10 +35,21 @@ class Account extends React.Component{
         })
     }
 
+    showPastDate=()=>{
+        let status = this.state.showDates
+        this.setState({
+            showDates: !status
+        })
+    }
+
     renderActivities=()=>{
         if(this.state.activities.length >0){
             return this.state.activities.map(activity =>{
-                return <SelectionCard activity={activity.attributes} addActivityToDate={''} random='true'/>
+                return <>
+                <Col>
+                <SelectionCard activity={activity.attributes} addActivityToDate={''} random='true' foodOnly={'past-food-date'}/>
+                </Col>
+                </>
             })
         }
     }
@@ -46,21 +58,22 @@ class Account extends React.Component{
         if(this.state.dates.length >0){
             return this.state.dates.map(date=>{
                 if(date.attributes.activities.length === 1){
-                    return <div>
-                        <Container fluid>
-                            <Row className="justify-content-md-center">
+                    return <>
+                            {/* <Row className="justify-content-md-center"> */}
+                                <Col>
                                 <PastDate date={date.attributes} type='food'/>
-                            </Row>
-                        </Container>
-                        </div>
+                                </Col>
+                            {/* </Row> */}
+                        </>
                 }else{
-                    return<div>
-                        <Container fluid>
-                            <Row className="justify-content-md-center">
+                    return <>
+                            {/* <Row className="justify-content-md-center"> */}
+                            <Col>
+                             
                                 <PastDate date={date.attributes} type='food-activity'/>
-                            </Row>
-                        </Container>
-                    </div> 
+                                </Col>
+                            {/* </Row> */}
+                        </> 
                 }
             })
         }
@@ -69,19 +82,29 @@ class Account extends React.Component{
             if(this.state.loading){
                 return <LoaderWheel/>
             }
-        return <div>
-            <h2>Welcome {this.props.user.username}, this is your Account</h2>
-            <h4><u>Dates you have saved</u></h4>
+        return <Container fluid>
+            <h1>Welcome {this.props.user.username}!</h1>
+            {this.state.showDates ? <Button variant='primary' size='lg' onClick={this.showPastDate}>Hide Dates!</Button> :<Button variant='primary' size='lg' onClick={this.showPastDate}>See Dates You Have Saved!</Button>}
+            
+            <Collapse in={this.state.showDates}>
+                <div>
+            <Row>
             {this.renderDates()}
-            <h4>All Food and Places you have saved</h4>
-            <ul> Total places = {this.state.activities.length}
+            </Row>
+            </div>
+            </Collapse>
+            <hr></hr>
+            <h1>Wow! You have been to {this.state.activities.length} {this.state.activities.length === 1 ? 'place!': 'places!'}</h1>
+             When you save a date, all the places you have been will show up below. Don't worry if you go to a place more than once. We will only show the place once. Now go out and explore!
+                <br></br>
+                <br></br>
                 <Container>
-                    <Row className="justify-content-md-center">
+                    <Row className="justify-content-md-center" lg={3} md={2} xs={1}>
                         {this.renderActivities()}
                     </Row>
                 </Container>
-            </ul>
-        </div>
+            
+        </Container>
         }
 }
 
